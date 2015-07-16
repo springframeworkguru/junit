640ab0e0e660aaf2893139b39aba7a44c7dece68
Ximanta <ximanta.sarma@gmail.com>
2015-07-16 17:24:54 +0530

    Modified EmailIDCreatorTest.java to include Assume.

--- src/main/test/guru/springframework/unittest/parameterized/EmailIDCreatorTest.java
+++ src/main/test/guru/springframework/unittest/parameterized/EmailIDCreatorTest.java
@@ -12,22 +12,28 @@ import java.util.Arrays;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.*;
+import static org.junit.Assume.assumeNotNull;
+import static org.junit.Assume.assumeThat;
 
 @RunWith(Theories.class)
 public class EmailIDCreatorTest {
 
     @DataPoints
     public static String[] names() {
-        return new String[]{"first","second","abc","123"};
+        return new String[]{"first","second","abc","123",null};
     }
     @DataPoint
     public static String name="mary";
     /*Generated Email ID returned by EmailIdUtility.createEmailID must contain first part and second part passed to it*/
     @Theory
     public void testCreateEmailID(String firstPart, String secondPart) throws Exception {
-        System.out.println(String.format("Testing with %s and %s",firstPart,secondPart));
+        System.out.println(String.format("Testing with %s and %s", firstPart, secondPart));
+        assumeNotNull(firstPart, secondPart);
+        /*Same assumptions as assumeNotNull(). Added only  to demonstrate usage of assertThat*/
+        assumeThat(firstPart, notNullValue());
+        assumeThat(secondPart, notNullValue());
         String actual= EmailIdUtility.createEmailID(firstPart,secondPart);
-        System.out.println(String.format("Actual: %s \n",actual));
+        System.out.println(String.format("Actual: %s \n", actual));
         assertThat(actual, is(allOf(containsString(firstPart), containsString(secondPart))));
     }
 }
